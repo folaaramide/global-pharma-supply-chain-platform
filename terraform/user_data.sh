1d065e9a95ebf68990a64f1e66b07ab68ca0ad56
folaaramide <afolabiaramide@gmail.com>
2026-06-22 20:18:43 +0100

    Fix user_data and GitHub Actions

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -2,35 +2,23 @@
 
 dnf update -y
 
-dnf install -y git
-
-dnf install -y python3
+dnf install -y git python3
 
 python3 -m ensurepip --upgrade
 
-python3 -m pip install flask
-
-python3 -m pip install flask-cors
-
-python3 -m pip install pymysql
+python3 -m pip install flask flask-cors pymysql
 
 
-echo 'export DB_HOST=global-pharma-supply-chain-db.cvsoquo865nl.eu-west-2.rds.amazonaws.com' >> /home/ssm-user/.bashrc
-
-echo 'export DB_USER=admin' >> /home/ssm-user/.bashrc
-
-echo 'export DB_PASSWORD=Password123!' >> /home/ssm-user/.bashrc
-
-echo 'export DB_NAME=pharmadb' >> /home/ssm-user/.bashrc
+export DB_HOST="global-pharma-supply-chain-db.cvsoquo865nl.eu-west-2.rds.amazonaws.com"
+export DB_USER="admin"
+export DB_PASSWORD="Password123!"
+export DB_NAME="pharmadb"
 
 
-export DB_HOST=global-pharma-supply-chain-db.cvsoquo865nl.eu-west-2.rds.amazonaws.com
-
-export DB_USER=admin
-
-export DB_PASSWORD='Password123!'
-
-export DB_NAME=pharmadb
+echo "export DB_HOST=$DB_HOST" >> /home/ssm-user/.bashrc
+echo "export DB_USER=$DB_USER" >> /home/ssm-user/.bashrc
+echo "export DB_PASSWORD=$DB_PASSWORD" >> /home/ssm-user/.bashrc
+echo "export DB_NAME=$DB_NAME" >> /home/ssm-user/.bashrc
 
 
 cd /home/ssm-user
@@ -42,6 +30,12 @@ git clone https://github.com/folaaramide/global-pharma-supply-chain-platform.git
 fi
 
 
-cd global-pharma-supply-chain-platform/app
+cd /home/ssm-user/global-pharma-supply-chain-platform/app
 
-nohup python3 app.py > flask.log 2>&1 &
+
+nohup env \
+DB_HOST=$DB_HOST \
+DB_USER=$DB_USER \
+DB_PASSWORD=$DB_PASSWORD \
+DB_NAME=$DB_NAME \
+python3 app.py > /home/ssm-user/flask.log 2>&1 &
